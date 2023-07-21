3ffc358f840b6645011297db6d69a5c6319a4941
MDMahidul <mmbmahidul007@gmail.com>
2023-07-21 21:41:26 +0600

    created react app and install all the dependencies and created pages and layouts

---
+++ src/pages/ChefCard/ChefCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChefCard.css'
+
+const ChefCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ChefCard;
---
+++ src/pages/Shared/ActiveLink/ActiveLink.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ActiveLink.css'
+
+const ActiveLink = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ActiveLink;
