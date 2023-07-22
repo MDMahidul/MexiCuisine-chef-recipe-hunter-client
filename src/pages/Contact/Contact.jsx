0f3e454469cb665266e412b519253e63cd19a1b6
MDMahidul <mmbmahidul007@gmail.com>
2023-07-22 17:07:39 +0600

    added two more section

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Button, Container,Form } from 'react-bootstrap';
 
 const Contact = () => {
     return (
-      <div>
-        <h1 className="section-header text-center pb-5">Book</h1>
-      </div>
+      <Container className='mb-5 pb-5'>
+        <h1 className="section-header text-center mt-5 pb-5">Contact Us</h1>
+        <Form className='w-75 mx-auto shadow p-5'>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label className='fw-bold gray'>Your Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter your name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label className='fw-bold gray'>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter your email address" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label className='fw-bold gray'>Message</Form.Label>
+            <Form.Control as="textarea" rows={3}  placeholder='Write your message'/>
+          </Form.Group>
+          <Button>Submit</Button>
+        </Form>
+      </Container>
     );
 };
 
--- src/pages/Testimonial/Testimonial.jsx
+++ src/pages/Testimonial/Testimonial.jsx
@@ -1,10 +1,119 @@
 import React from 'react';
+import { Carousel, Container } from 'react-bootstrap';
+import './Testimonial.css';
 
 const Testimonial = () => {
     return (
-      <div>
-        <h1 className="section-header text-center pb-5">Customers Review</h1>
-      </div>
+      <Container className="text-center">
+        <h1 className="section-header text-center mt-5 pb-3">User Review</h1>
+        <Carousel className="pb-5 my-4 pt-3">
+          <Carousel.Item interval={3000}>
+            <div className="">
+              <div>
+                <img
+                  className="testi-img"
+                  src="https://www.pngkit.com/png/full/639-6399637_henry-circle-gentleman.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h5 className="reviewer pt-3">Luis Enrique</h5>
+                <p className="review">
+                  MexiCuisine has become my go-to website for authentic Mexican
+                  recipes! The collection of recipes is diverse and caters to
+                  all levels of cooking expertise. Whether you're a beginner or
+                  a seasoned cook, you'll find something delicious to try. The
+                  step-by-step instructions with accompanying images make the
+                  cooking process a breeze. I love the way the website is
+                  organized, making it easy to search for specific dishes or
+                  ingredients. Kudos to MexiCuisine for bringing the flavors of
+                  Mexico right to my kitchen!
+                </p>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <div className="">
+              <div>
+                <img
+                  className="testi-img"
+                  src="https://www.pngitem.com/pimgs/m/287-2871712_women-circle-image-png-transparent-png.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h5 className="reviewer pt-3">Sofia Hayat</h5>
+                <p className="review">
+                  As a food enthusiast and Mexican cuisine lover, MexiCuisine is
+                  a hidden gem! The website offers a delightful variety of
+                  traditional and modern Mexican dishes that leave my taste buds
+                  craving for more. The featured chef profiles add a personal
+                  touch to the recipes and offer valuable insights. The
+                  user-friendly interface and responsive design make it a joy to
+                  browse the site on any device. I've tried several recipes, and
+                  each one has turned out exceptionally flavorful and authentic.
+                  I highly recommend MexiCuisine to all foodies looking to
+                  explore the vibrant flavors of Mexico.
+                </p>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <div className="">
+              <div>
+                <img
+                  className="testi-img"
+                  src="https://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h5 className="reviewer pt-3">David Milan</h5>
+                <p className="review">
+                  MexiCuisine is a lifesaver for me! As someone with dietary
+                  restrictions, finding a recipe website that caters to my needs
+                  can be challenging. However, MexiCuisine goes above and beyond
+                  by providing options for various dietary preferences,
+                  including gluten-free and vegetarian dishes. The website's
+                  layout is intuitive, making it easy to navigate and filter
+                  recipes based on my requirements. I appreciate the nutritional
+                  information provided for each recipe, helping me make informed
+                  choices. The mouthwatering images accompanying each recipe are
+                  an added bonus. Thank you, MexiCuisine, for making my culinary
+                  journey enjoyable and inclusive
+                </p>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item interval={3000}>
+            <div className="">
+              <div>
+                <img
+                  className="testi-img"
+                  src="https://www.pngitem.com/pimgs/m/128-1284293_marina-circle-girl-picture-in-circle-png-transparent.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h5 className="reviewer pt-3">Salena Gomez</h5>
+                <p className="review">
+                  MexiCuisine has transformed my cooking experience and ignited
+                  my passion for Mexican cuisine. The curated selection of
+                  recipes covers a wide range of dishes, from beloved classics
+                  to innovative twists. The website's attention to detail, such
+                  as cooking tips and ingredient substitutions, ensures that
+                  even beginners can confidently prepare a scrumptious Mexican
+                  meal. The interactive community aspect, with user ratings and
+                  reviews, fosters a sense of community among food enthusiasts.
+                  MexiCuisine has undoubtedly become an indispensable resource
+                  in my kitchen. I'm grateful for the inspiration and culinary
+                  adventure it has provided!
+                </p>
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
     );
 };
 
